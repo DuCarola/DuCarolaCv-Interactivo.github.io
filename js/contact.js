b833281579839e6bce91327904c9eedffa1c2f24
DuCarola <ducarola@gmail.com>
2023-03-01 22:22:30 -0300

    Ultimos cambios en css y html

--- js/contact.js
+++ js/contact.js
@@ -4,9 +4,15 @@ btnEnviar.addEventListener("click", submitComment);
 let userName = document.querySelector("#userName");
 let comment = document.querySelector("#comment");
 let userComment = document.querySelector("#userComment");
+let contactContainer = document.querySelector("#contactContainer")
+let backgroundColor = document.querySelector("#backgroundColor")
+backgroundColor.addEventListener("click", changeBackgroundColor)
 
 function submitComment(){
     userComment.innerHTML += `<p class="contactInfoText"> ${userName.value} comentó: ${comment.value} </p>`
    
+}
+function changeBackgroundColor(){
+    contactContainer.style.backgroundColor = "darkgreen";
 }
 
